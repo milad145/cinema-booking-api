9eedb38d124e4df18f6c81ec620f6a24c6bd80d2
milad145 <miladaslani1991@gmail.com>
2025-03-28 16:57:49 +0330

    init project

---
+++ libs/init.js
@@ -0,0 +1,32 @@
+import http from 'http';
+
+import express from "express";
+
+import routes from "./routes/index.js";
+
+export const initiateExpress = async (config) => {
+
+    const app = express();
+    app.disable('x-powered-by');
+    app.use(function (req, res, next) {
+        res.removeHeader("X-Powered-By");
+        next();
+    });
+
+    app.set("trust proxy", 1)
+
+    app.use(express.text());
+    app.use(express.urlencoded({limit: "0.4mb", extended: true}));
+    app.use(express.json());
+
+    routes(app);
+
+    const httpServer = http.createServer({}, app);
+
+    httpServer.listen(config.port, function () {
+        console.log(`🚀 Server ready at http://localhost:${config.port}`);
+    }).on('error', function (err) {
+        console.error("HTTPS server error:", err.message);
+        process.exit(1);
+    });
+}
