a9a343051bbbf86c1edb176f12e64838177271d2
milad145 <miladaslani1991@gmail.com>
2025-03-28 16:57:16 +0330

    is login middleware

---
+++ libs/modules/middlewares.js
@@ -0,0 +1,23 @@
+import {errorCode} from "./errorHandler.js";
+import {setErrorResponse, validateJWTToken} from "./assist.js";
+
+export const isLogin = async (req, res, next) => {
+    let {authorization} = req.headers;
+    const token = authorization && authorization.split(' ')[1];
+
+    if (!token) {
+        return setErrorResponse(req, res, errorCode(401))
+    } else if (authorization && authorization.startsWith('Bearer')) {
+        try {
+            const user = await validateJWTToken( token);
+            if (user && user._id) {
+                req.user = user;
+                next()
+            } else {
+                setErrorResponse(req, res, errorCode(401))
+            }
+        } catch (e) {
+            setErrorResponse(req, res, errorCode(401))
+        }
+    }
+}
